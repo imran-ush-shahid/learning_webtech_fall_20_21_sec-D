46aa79adb7862459d80b01ce56be3ba386b1011a
imran-ush-shahid <imranshabi67@gmail.com>
2020-11-02 10:53:38 +0600

    first commit

---
+++ LAB_TASK 3.1_php/name/TASK_2_CURRENT_PAGE/nameinput.php
@@ -0,0 +1,30 @@
+<?php
+	
+	
+
+	if(isset($_REQUEST['submit'])){
+		$name = $_REQUEST['name'];
+		
+		if($name == ""){
+			echo " field required...";
+		}else{
+			echo = $name;
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Name</title>
+</head>
+<body>
+	<form method="post" action="nameinput.php" >
+		<fieldset>
+			<legend>Name</legend>
+			<input type="text" name="name" value=" ">
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
