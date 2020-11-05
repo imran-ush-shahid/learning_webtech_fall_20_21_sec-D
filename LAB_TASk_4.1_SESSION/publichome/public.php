f28e7091f10847fea2a0cd2679b0417c902e0572
imran-ush-shahid <imranshabi67@gmail.com>
2020-11-05 10:35:45 +0600

    first commit

---
+++ LAB_TASk_4.1_SESSION/publichome/public.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>public home</title>
+</head>
+<body>
+<table width="900px"border="1" aligen="center">
+	<tr>
+		<td width="100px"><img src="x.jpg"width=100px" height="100px"></td>
+		<td colspan="2" align="center">
+			<a href="home.php">HOME</a>
+			<a href="login.php">Login</a>
+			<a href="registration.php">Registration</a>
+		</td>
+	</tr>
+	<tr height="400px">
+		<h1>welcome to xcompany</h1>
+	</tr>
+	<tr height="50px">
+		<td colspan="3" align="center">
+			copyright@2017
+		</td>
+	</tr>
+</table>
+
+
+</body>
+</html>
