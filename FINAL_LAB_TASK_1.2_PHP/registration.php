cda9ac8680d24a68f5fb5984a104cb40e1120329
imran-ush-shahid <imranshabi67@gmail.com>
2020-12-02 08:36:30 +0600

    first commit

---
+++ FINAL_LAB_TASK_1.2_PHP/registration.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Shop - Registration</title>
+</head>
+<body>
+    
+    <br><br><br><br><br><br><br>
+    
+    <center>
+        
+        <fieldset>
+            
+            <legend><b>Registration</b></legend>
+            
+            <form action="../controller/registrationCheck.php" method="post">
+            
+            Name : <input type="text" name="name"><br><br>
+            Contact No : <input type="text" name="contactNo"><br><br>
+            UserName : <input type="text" name="username"><br><br>
+            password : <input type="password" name="password"><br><br>
+            <input type="submit" name="submit" value="Register">
+        </form>
+            
+        </fieldset>
+        
+    </center>
+    
+</body>
+</html>
